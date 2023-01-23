a67f7ff089fb8085eb58d26544e8b560a8fa9b96
Darwin073100 <edwinegq@outlook.com>
2023-01-23 00:13:36 -0600

    schema => students 👨‍🎓

---
+++ schema/student.schema.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const name = Joi.string();
+const lastName = Joi.string();
+const numControl = Joi.number().integer();
+const address = Joi.string();
+const email = Joi.string().email();
+const dateBirth = Joi.string();
+
+const createStudentSchema = Joi.object({
+  name: name.required(),
+  lastName: lastName.required(),
+  numControl: numControl.required(),
+});
+
+const updateStudentSchema = Joi.object({
+  name,
+  lastName,
+  numControl,
+  address,
+  email,
+  dateBirth,
+});
+
+module.exports = { createStudentSchema, updateStudentSchema };
