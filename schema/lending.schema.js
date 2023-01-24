598dd0f7dcec245c3640841b746fd16ca9cf2156
Darwin073100 <edwinegq@outlook.com>
2023-01-23 19:04:10 -0600

    schema of Joi finish

---
+++ schema/lending.schema.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const bookId = Joi.number().integer();
+const studentId = Joi.number().integer();
+const dateOut = Joi.date();
+const dateIn = Joi.date();
+
+const getLendingSchema = Joi.object({
+  id: id.required(),
+});
+
+const createLendingSchema = Joi.object({
+  bookId: bookId.required(),
+  studentId: studentId.required(),
+  dateOut: dateOut.required(),
+  dateIn: dateIn.required(),
+});
+
+const updateLendingSchema = Joi.object({
+  bookId,
+  studentId,
+  dateOut,
+  dateIn,
+});
+
+module.exports = {
+  getLendingSchema,
+  createLendingSchema,
+  updateLendingSchema,
+};
